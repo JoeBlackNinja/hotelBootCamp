f78e3cf1dbde75599a4b3a738dcbe18cd6cf6b84
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-03-27 15:57:47 -0600

    Uso de sequelize, se agregaro modelo usuario con password, ruta de escritura en postgres exitosa

--- Servidor/controller/LoginController.js
+++ Servidor/controller/LoginController.js
@@ -1,3 +1,11 @@
+const Sequelize = require('sequelize');
+const usuario = require('../models').Usuario;
+
 module.exports = {
-    
+    create(req,res){
+        return usuario.create({userName: req.params.userName, 
+            password: req.params.password})
+            .then(usuario => res.status(200).send(usuario))
+            .catch(error => res.status(400));
+    }
 }
--- Servidor/routes/index.js
+++ Servidor/routes/index.js
@@ -1,6 +1,10 @@
+const LoginController = require('../controller/LoginController');
 
 module.exports = (app) => {
     app.get('/api',(req,res) => {
         res.status(200).send('Cantilever... desde método GET');
     })
+
+    app.post('/user/create/userName/:userName/password/:password',
+        LoginController.create);
 }
